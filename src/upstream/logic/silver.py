cc73781eb34cc7737d06ea9f0ea31499d635e6da
Tetro <itsofirk@gmail.com>
2023-09-22 19:54:58 +0300

    Implemented load_table and clean_data

--- src/upstream/logic/silver.py
+++ src/upstream/logic/silver.py
@@ -6,12 +6,22 @@ import pyarrow as pa
 from pyarrow import parquet as pq
 
 
-def load_parquet(path: str) -> pd.DataFrame:
-    ...
+def load_table(path: str) -> pd.DataFrame:
+    return pd.read_parquet(path)
 
 
 def clean_data(df: pd.DataFrame) -> pd.DataFrame:
-    ...
+    df = filter_null_vins(df)
+    df = remove_rows_with_bad_manufacturer(df)
+    return df
+
+
+def filter_null_vins(df):
+    return df.dropna(subset='vin')
+
+
+def remove_rows_with_bad_manufacturer(df):
+    return df[df.manufacturer.str.strip() == df.manufacturer]
 
 
 def standardize_gear_position(df: pd.DataFrame) -> pd.DataFrame:
