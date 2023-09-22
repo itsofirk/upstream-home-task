fa7afceea4be8f6ae721a16d5b2282927edfb734
Tetro <itsofirk@gmail.com>
2023-09-22 14:22:05 +0300

    Added docstrings to get_messages and bronze Added logs to bronze

--- src/upstream/logic/bronze.py
+++ src/upstream/logic/bronze.py
@@ -12,7 +12,15 @@ from upstream.common.exceptions import ApiError
 logger = logging.getLogger(__name__)
 
 
-def get_messages(url, amount):
+def get_messages(url, amount) -> list[dict]:
+    """
+    get_messages fetches a list of messages from the API.
+
+    :param url: Specify the url of the api endpoint
+    :param amount: Specify how many messages to fetch
+    :return: A list of messages
+    """
+
     logger.info(f"Fetching {amount:,} messages...")
     resp = requests.get(url, params={'amount': amount})
 
@@ -36,6 +44,20 @@ def export_parquet(df, bronze_dir):
 
 
 def bronze(upstream_url, bronze_dir, messages_count=10_000):
+    """
+    The bronze function takes an upstream URL, a bronze directory, and a messages count.
+    It gets messages from the upstream URL, parses them into a dataframe, and exports that dataframe
+    to parquets in the bronze directory.
+
+    :param upstream_url: Specify the url of the upstream api
+    :param bronze_dir: Specify the directory where the parquet file will be stored
+    :param messages_count: The number of messages that are retrieved from the upstream api
+    """
+    logger.info("Starting the bronze stage...")
+    logger.debug("Getting messages...")
     messages = get_messages(upstream_url, messages_count)
+    logger.debug("Parsing messages...")
     df = parse_messages(messages)
+    logger.debug("Exporting data to bronze_dir...")
     export_parquet(df, bronze_dir)
+    logger.info("Bronze stage complete.")
