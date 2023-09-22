11c80d012e15c45c5cc809b53044d306f30efe4c
Tetro <itsofirk@gmail.com>
2023-09-22 21:36:49 +0300

    implemented the gear standardizing function

--- src/upstream/logic/silver.py
+++ src/upstream/logic/silver.py
@@ -1,9 +1,13 @@
 """
 silver stage logic
 """
+import numpy as np
 import pandas as pd
 import pyarrow as pa
 from pyarrow import parquet as pq
+
+GEAR_POSITION_MAPPING = {'NEUTRAL': 0, 'REVERSE': -1, None: np.nan,
+                         '-1': -1, '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6}
 
 
 def load_table(path: str) -> pd.DataFrame:
@@ -24,8 +28,14 @@ def remove_rows_with_bad_manufacturer(df):
     return df[df.manufacturer.str.strip() == df.manufacturer]
 
 
-def standardize_gear_position(df: pd.DataFrame) -> pd.DataFrame:
-    ...
+def standardize_gear_position(df: pd.DataFrame, gear_mapping) -> pd.DataFrame:
+    """
+    Takes a dataframe as input and returns the same dataframe with the gearPosition column standardized converted to
+    integers.
+    If a value is not found in the gear_mapping, it will be mapped to NaN.
+    """
+    df['gearPosition'] = df.gearPoistion.map(gear_mapping)
+    return df
 
 
 def export_parquet(df: pd.DataFrame, path: str) -> None:
