315e761633fdd9ea4468f5ef71150df44e820367
Tetro <itsofirk@gmail.com>
2023-09-22 18:39:39 +0300

    created a skeleton for silver logic module

--- src/upstream/logic/silver.py
+++ src/upstream/logic/silver.py
@@ -1,3 +1,26 @@
 """
 silver stage logic
 """
+import pandas as pd
+import pyarrow as pa
+from pyarrow import parquet as pq
+
+
+def load_parquet(path: str) -> pd.DataFrame:
+    ...
+
+
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    ...
+
+
+def standardize_gear_position(df: pd.DataFrame) -> pd.DataFrame:
+    ...
+
+
+def export_parquet(df: pd.DataFrame, path: str) -> None:
+    ...
+
+
+def silver(bronze_dir: str, silver_dir: str) -> None:
+    ...
