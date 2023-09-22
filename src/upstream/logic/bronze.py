0738af9ef5d0cd3a95d8a2ab3683ebb3cd2d220c
Tetro <itsofirk@gmail.com>
2023-09-22 12:43:54 +0300

    created a skeleton for the bronze stage

--- src/upstream/logic/bronze.py
+++ src/upstream/logic/bronze.py
@@ -7,3 +7,20 @@ import pandas
 from pyarrow import parquet as pq
 from datetime import datetime
 
+
+def get_messages(url, amount):
+    ...
+
+
+def parse_messages(messages):
+    ...
+
+
+def export_parquet(df, bronze_dir):
+    ...
+
+
+def bronze(upstream_url, bronze_dir, messages_count=10_000):
+    messages = get_messages(upstream_url, messages_count)
+    df = parse_messages(messages)
+    export_parquet(df, bronze_dir)
