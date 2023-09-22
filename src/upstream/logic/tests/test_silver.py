7c9fb7b37d725b71aa37155d0e6939d7c15b3afa
Tetro <itsofirk@gmail.com>
2023-09-22 23:06:56 +0300

    added a test case for the gear standardizing

--- src/upstream/logic/tests/test_silver.py
+++ src/upstream/logic/tests/test_silver.py
@@ -1,5 +1,6 @@
 import unittest
 import pandas as pd
+from pandas.testing import assert_frame_equal
 
 from upstream.logic import silver
 
@@ -18,7 +19,23 @@ class TestCleanData(unittest.TestCase):
             'manufacturer': ['A', 'B']
         })
         output = silver.clean_data(df)
-        self.assertTrue(expected.equals(output))
+        assert_frame_equal(expected, output)
+
+
+class TestStandardizeGearPosition(unittest.TestCase):
+    """
+    Test the standardize_gear_position function.
+    """
+    def test_standardize_gear_position(self):
+        df = pd.DataFrame({'gearPosition': ['1', None, 3, 'REVERSE', 'Unknown']})
+        expected = pd.DataFrame({'gearPosition': [1, pd.NA, pd.NA, -1, pd.NA]}, dtype='Int64')
+        gear_mapping = {
+            '1': 1,
+            None: pd.NA,
+            'REVERSE': -1
+        }
+        output = silver.standardize_gear_position(df, gear_mapping)
+        assert_frame_equal(expected, output)
 
 
 if __name__ == '__main__':
