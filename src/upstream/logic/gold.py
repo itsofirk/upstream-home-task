1e192e6a38749409cb6d07ccbe50f1eef091efec
Tetro <itsofirk@gmail.com>
2023-09-22 23:48:53 +0300

    created a skeleton for gold

--- src/upstream/logic/gold.py
+++ src/upstream/logic/gold.py
@@ -1,3 +1,22 @@
 """
 gold stage logic
 """
+import pandas as pd
+
+from upstream import datalake
+
+
+def load_silver(silver_dir: str) -> pd.DataFrame:
+    return datalake.load_parquet(silver_dir)
+
+
+def generate_vin_last_state_report(silver_data: pd.DataFrame):
+    ...
+
+
+def generate_top_10_fastest_vehicles_report(silver_data):
+    ...
+
+
+def gold(silver_dir: str, gold_dir: str):
+    ...
