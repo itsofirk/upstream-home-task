10720b2877703cc1c360cf2397ebec3a364cd3d4
Tetro <itsofirk@gmail.com>
2023-09-22 23:41:12 +0300

    fixed datalake unittests

--- src/upstream/tests/test_datalake.py
+++ src/upstream/tests/test_datalake.py
@@ -4,22 +4,22 @@ from unittest.mock import patch
 import pandas as pd
 import pyarrow as pa
 
+from upstream import datalake
 from upstream.common.exceptions import DataLakeError
-from upstream.logic import bronze
 
 
 class TestExportParquet(unittest.TestCase):
     """
     Test the export_parquet function.
     """
-
     @patch('pyarrow.parquet.write_to_dataset')
-    def test_export_parquet_success(self, mock_write_to_dataset):
+    @patch('upstream.datalake.is_empty')
+    def test_export_parquet_success(self, mock_is_empty, mock_write_to_dataset):
         data = {'timestamp': ['2023-09-22 12:00:00', '2023-09-22 13:00:00'],
                 'data': [1, 2]}
         df = pd.DataFrame(data)
-
-        bronze.export_parquet(df, 'bronze_dir')
+        mock_is_empty.return_value = True
+        datalake.export_parquet(df, 'bronze_dir', partition_cols=['date', 'hour'])
 
         expected_table = pa.Table.from_pandas(df)
         mock_write_to_dataset.assert_called_once_with(expected_table, root_path='bronze_dir',
@@ -34,14 +34,20 @@ class TestExportParquet(unittest.TestCase):
         df = pd.DataFrame(data)
 
         with self.assertRaises(DataLakeError):
-            bronze.export_parquet(df, 'bronze_dir')
+            datalake.export_parquet(df, 'bronze_dir')
 
         mock_write_to_dataset.assert_not_called()
 
-    def test_missing_field_in_schema(self):
+    @patch('upstream.datalake.is_empty')
+    def test_missing_field_in_schema(self, mock_is_empty):
         data = {'timestamp': ['2023-09-22 12:00:00', '2023-09-22 13:00:00'],
                 'data': [1, 2]}
         df = pd.DataFrame(data)
+        mock_is_empty.return_value = True
 
         with self.assertRaises(KeyError):
-            bronze.export_parquet(df, '/dev/null')
+            datalake.export_parquet(df, '/dev/null', partition_cols=['not_in_schema'])
+
+
+if __name__ == '__main__':
+    unittest.main()
