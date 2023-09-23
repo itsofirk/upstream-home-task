aaf3adeec6dc3032b9758715e95a48be6fba2864
Tetro <itsofirk@gmail.com>
2023-09-23 14:24:50 +0300

    Added logs to set_up_app_db

--- src/upstream/infrastructure/db.py
+++ src/upstream/infrastructure/db.py
@@ -1,23 +1,27 @@
 import os
+import logging
 import sqlite3
 
-from upstream.common.config import InfraConfig
 from upstream.infrastructure import sql_queries as SQL
 
+logger = logging.getLogger(__name__)
 SQLITE_FILENAME = 'upstream.db'
-db_path = os.path.join(InfraConfig.app_data_dir, SQLITE_FILENAME)
 
 
-def set_up_data_lake_db():
+def set_up_app_db(app_data_dir):
+    db_path = os.path.join(app_data_dir, SQLITE_FILENAME)
+    logger.info("Setting up database...")
     con = sqlite3.connect(db_path)
     cur = con.cursor()
 
     # create file_monitoring table
+    logger.debug("Creating file_monitoring table...")
     cur.execute(SQL.CREATE_FILE_MONITORING_TABLE)
-
     # create stage_execution table
+    logger.debug("Creating stage_execution table...")
     cur.execute(SQL.CREATE_STAGE_EXECUTION_TABLE)
 
     # close connection
     cur.close()
     con.close()
+    logger.info("Database set up.")
