2fcf0f8c1e097baca4cf2a45d43b101f14712c09
Tetro <itsofirk@gmail.com>
2023-09-23 18:32:47 +0300

    removed flask

--- src/app.py
+++ src/app.py
@@ -1,4 +1,3 @@
-from flask import Flask, jsonify
 import logging
 
 from upstream.common.config import AppConfig, DatalakeConfig, UpstreamConfig
@@ -8,33 +7,10 @@ from upstream.logic import bronze, silver, gold
 
 logger = logging.getLogger(AppConfig.app_name)
 
-app = Flask(AppConfig.app_name)
 file_watcher = FileWatcher(DatalakeConfig.bronze_name, DatalakeConfig.bronze_path(), DatalakeConfig.silver_name,
                            DatalakeConfig.silver_path())
 
 
-@app.route('/bronze')
-def run_bronze():
-    logger.info("Running bronze stage...")
-    bronze(UpstreamConfig.url, DatalakeConfig.bronze_path(), UpstreamConfig.amount)
-    return jsonify(response="Bronze stage done!", status=200, mimetype='application/json')
-
-
-@app.route('/silver')
-def run_silver():
-    logger.info("Running silver stage...")
-    silver(DatalakeConfig.bronze_path(), DatalakeConfig.silver_path())
-    return jsonify(response="Silver stage done!", status=200, mimetype='application/json')
-
-
-@app.route('/gold')
-def run_gold():
-    logger.info("Running gold stage...")
-    gold(DatalakeConfig.silver_path(), DatalakeConfig.gold_path())
-    return jsonify(response="Gold stage done!", status=200, mimetype='application/json')
-
-
-@app.route('/process')
 def run_process():
     logger.info("New process request")
     logger.debug("Running bronze...")
@@ -43,7 +19,7 @@ def run_process():
     silver(DatalakeConfig.bronze_path(), DatalakeConfig.silver_path(), )
     logger.debug("Running gold...")
     gold(DatalakeConfig.silver_path(), DatalakeConfig.gold_path())
-    return jsonify(response="Done!", status=200, mimetype='application/json')
+    return dict(response="Done!", status=200, mimetype='application/json')
 
 
 if __name__ == '__main__':
@@ -51,5 +27,4 @@ if __name__ == '__main__':
     set_up_local_data_lake(DatalakeConfig.root_path)
     file_watcher.start()
     logger.info("Starting application...")
-    app.run(host='0.0.0.0', port=5000)
     logger.info("Application started.")
