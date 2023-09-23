275ca731d9bb282d70eaf961d068da4141dedf90
Tetro <itsofirk@gmail.com>
2023-09-23 20:39:12 +0300

    removed old config

--- src/upstream/common/config.py
+++ src/upstream/common/config.py
@@ -14,17 +14,6 @@ class AppConfig:
     app_name = config.get('general', 'app_name')
 
 
-class InfraConfig:
-    host = config.get('infrastructure', 'redis_host')
-    port = config.getint('infrastructure', 'redis_port')
-    db = config.getint('infrastructure', 'redis_db')
-    app_data_dir = config.get('infrastructure', 'app_data_dir')
-
-    @classmethod
-    def get_redis_broker(cls):
-        return f'redis://{cls.host}:{cls.port}/{cls.db}'
-
-
 class UpstreamConfig:
     url = config.get('upstream', 'url')
     amount = config.getint('upstream', 'amount')
