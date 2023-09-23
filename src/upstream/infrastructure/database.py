8f14587a34e3a6bb18f01d6852159637eecda000
Tetro <itsofirk@gmail.com>
2023-09-23 16:42:17 +0300

    moved service related functions from database.py to service.py

--- src/upstream/infrastructure/database.py
+++ src/upstream/infrastructure/database.py
@@ -31,27 +31,3 @@ class Database:
         with Database.connection(self.db_path) as con:
             cur = con.execute(query, tuple(query_params))
             return cur.fetchall()
-
-    def start_new_job(self, stage_name) -> int:
-        """
-        Takes a stage name, creates a new job in the database and returns the id of the newly created job
-        The start_time is set to the timestamp when this function was called
-        :param stage_name: Create a new job in the database
-        :return: The job_id of the new job
-        """
-        logger.debug("Starting new job...")
-        start_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        return self.execute(SQL.START_NEW_JOB, stage_name, start_time)[0]
-
-    def end_job(self, job_id):
-        """
-        Sets the end_time in the row matching the job_id in the jobs table
-        The end_time is set to the timestamp when this function was called
-        :param self: Represent the instance of the class
-        :param job_id: Identify the job that is being ended
-        :return: The end time of the job
-        :doc-author: Trelent
-        """
-        logger.debug("Ending job...")
-        end_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        self.execute(SQL.END_JOB, end_time, job_id)
